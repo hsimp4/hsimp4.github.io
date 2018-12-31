b041694a0775dc7e2b90fdc7ddeb4f9b6452670d
Hunter Simpson <hsimpson270@gmail.com>
2018-12-31 14:39:21 -0600

    Optimized home page for page load and accesbility.

--- Scripts/Shared.js
+++ Scripts/Shared.js
@@ -3,4 +3,13 @@ $(function () {
         var $nav = $(".navbar");
         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+
+    $('.lazy').Lazy({
+        threshold: 250,
+        scrollDirection: 'vertical',
+        visibleOnly: true,
+        onError: function(element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
 });
